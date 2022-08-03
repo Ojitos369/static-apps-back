2aef9b035bbe12a202052b3ea73d695ac90239ed
ojitos369 <ojitos369@gmail.com>
2022-08-02 20:13:24 -0500

    start rework

--- matrices/secret.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'django-insecure-6(jgp#!x7ael*wx_kvhlu)$17ji_#5c^h+fn*80otgo2i=qik8'
--- static_apps/asgi.py
+++ static_apps/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for matrices project.
+ASGI config for static_apps project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matrices.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'static_apps.settings')
 
 application = get_asgi_application()
