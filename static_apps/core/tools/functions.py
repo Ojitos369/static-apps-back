2a5472077ef7dddfc287cdc95141c9b563fb01d3
ojitos369 <ojitos369@gmail.com>
2022-08-09 10:22:19 -0500

    error message with function

--- static_apps/core/tools/functions.py
+++ static_apps/core/tools/functions.py
@@ -1,15 +1,15 @@
 def show_error(e, send_email = False):
     import os
     import datetime
-    exc_type, exc_obj, exc_tb = os.sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)
-    rute, file_name = fname[0], fname[1]
-    fname = file_name
-    file = rute + '/' + fname
+    info_exc = os.sys.exc_info()
+    exc_type, exc_obj, exc_tb = info_exc
+    file = exc_tb.tb_frame.f_code.co_filename
+    function_data = str(exc_tb.tb_frame).split()
+    function_data = function_data[-1][:-1]
     now = datetime.datetime.now()
     now = now.strftime("%d/%m/%Y %H:%M:%S")
     
-    error = f'ERROR INFO\nTipo: {exc_type}\nArchivo: {file}\nlinea: {exc_tb.tb_lineno}\nError: {e}\nFecha: {now}'
+    error = f'ERROR INFO\nTipo: {exc_type}\nArchivo: {file}\nFuncion: {function_data}\nLinea: {exc_tb.tb_lineno}\nError: {e}\nFecha: {now}'
     
     if send_email:
         from static_apps.core.emails.emails import EmailError
